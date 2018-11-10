a3d0e65cd54a2672b4c39e13c19a25d3bed11642
Jennifer Gardner <jgardn92@uw.edu>
2018-11-09 21:26:38 -0800

    Script Quiz

--- xFISH546/ScriptQuiz/Scriptabbr.sh
+++ xFISH546/ScriptQuiz/Scriptabbr.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-set -e
-set -u
-set -o pipefail
-#setup according to the book
-mkdir ./zips
-mkdir ./unzip
-mkdir ./unzip/Stack1
-mkdir ./ToUpload/
-#make relevant directories
-curl eagle.fish.washington.edu/fish546/jenny/Stack105700.Naso%20unicornis.zip > ./zips/Stack1.zip
-unzip ./zips/Stack1.zip -d ./unzip/Stack1/
-#download and unzip the data
 if test -f ./unzip/Stack1/Info.txt
 then
     dos2unix ./unzip/Stack1/Info.txt
@@ -33,8 +20,5 @@ else
     echo "Not UWFC"
 fi
 name="${Museum}-${Collection}-${Number}"
-#extract
 mv ./unzip/Stack1/*.log ./ToUpload/"${name}.log"
-unzip ./unzip/Stack1/Stack.zip -d ./unzip/tmp
-for f in unzip/tmp/*.jpg ; do mv $f ${f//Image/$name} ; done
-zip -r "${name}.zip" unzip/tmp > ./ToUpload/"${name}.zip"
+mv ./unzip/Stack1/Stack.zip ./ToUpload/"${name}.zip"
